16ed3f761b4052b5f5ee2b1f1b8afceba8b11dae
milkymilky0116 <sjlee990129@gmail.com>
2023-09-27 19:27:21 +0900

    text templates

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
+var tmp *template.Template
+
+func init() {
+	tmp = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
 func main() {
-	fmt.Println("HI!")
+	err := tmp.ExecuteTemplate(os.Stdout, "one.gohtml", "one")
+	handleErr(err)
+	err = tmp.ExecuteTemplate(os.Stdout, "two.gohtml", "two")
+	handleErr(err)
+	err = tmp.ExecuteTemplate(os.Stdout, "three.gohtml", "three")
+	handleErr(err)
+}
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
