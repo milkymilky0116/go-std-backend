bb526a49d37aae4ccd5759908d3a665e94bd6966
milkymilky0116 <sjlee990129@gmail.com>
2023-10-06 17:54:08 +0900

    Initializing Project

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
 # Go Standard Library Backend
 
-# Usage of Gist:
-
-- _-tmp_ boolean
-  - Whether running application on Template Mode
-- _-dbname_ string
-  - Database Name
-- _-password_ string
-  - Database User Password
-- _-port_ int
-  - MySQL port (default 3306)
-- _-user_ string
-  - Database Username (default "root")
-
-![CLI](https://github.com/milkymilky0116/go-std-backend/assets/84823612/d199c740-d382-422b-b6b3-0852e107c84d)
-
 # 개발기록
 
 - [Go 기본 라이브러리 백엔드 - 개발기록 1](https://milkymilky0116.github.io/posts/go_std_backend_1/)
--- main.go
+++ main.go
@@ -1,71 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/fatih/color"
-	"github.com/go-playground/form"
-	"github.com/milkymilky0116/go-std-backend/cmd/cli"
-	"github.com/milkymilky0116/go-std-backend/cmd/db"
-	"github.com/milkymilky0116/go-std-backend/cmd/web"
-	"github.com/milkymilky0116/go-std-backend/internal/models"
-)
+import "fmt"
 
 func main() {
-
-	blue := color.New(color.BgBlue).SprintFunc()
-	red := color.New(color.BgRed).SprintFunc()
-
-	infoLog := log.New(os.Stdout, blue("INFO\t"), log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stdout, red("ERROR\t"), log.Ldate|log.Ltime|log.Lshortfile)
-
-	c, err := cli.ParseArgs(os.Stderr, os.Args[1:])
-	if err != nil {
-		infoLog.Println(err)
-		os.Exit(1)
-	}
-
-	db, err := db.DBinit(c)
-	if err != nil {
-		errorLog.Println(err)
-		os.Exit(1)
-	}
-	infoLog.Println("DB Connected!")
-	defer db.Close()
-	formDecoder := form.NewDecoder()
-	app := &web.Application{
-		ServerLogger:   infoLog,
-		ErrorLogger:    errorLog,
-		Gists:          &models.GistModel{DB: db},
-		Users:          &models.UserModel{DB: db},
-		IsTemplateMode: c.IsTemplateMode,
-		FormDecoder:    formDecoder,
-	}
-
-	mux := app.InitRoutes()
-	defaultMode := "API Mode"
-	if app.IsTemplateMode {
-		defaultMode = "Template Mode"
-		templateCache, err := web.NewTemplateCache()
-		if err != nil {
-			errorLog.Fatal(err)
-		}
-		app.TemplateCache = templateCache
-	}
-
-	srv := &http.Server{
-		Addr:     ":4000",
-		ErrorLog: errorLog,
-		Handler:  mux,
-	}
-
-	startLog := fmt.Sprintf("Server Starting on :4000 / %s", defaultMode)
-	infoLog.Println(startLog)
-	err = srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Init Project")
 }
